a3a53e2deab86cd324c890be8da33a3b1da98fb0
Matteo Baggio <mbaggio@MacBook-Pro-di-Matteo.local>
2014-02-26 14:24:51 +0100

    First commit of UkkioSDK

---
+++ README.md
@@ -0,0 +1,310 @@
+
+# Ukkio SDK
+
+SDK to connect your game to Ukkio (http://developer.ukk.io).
+
+## Getting started
+
+- Install [ukkio-cli](https://github.com/ukkio/ukkio-cli) tool.
+
+	```
+	$ npm install ukkio-cli -g
+	```
+
+- Install SDK as you prefer:
+
+	```js
+	bower install ukkio-sdk
+	```
+	
+	or
+
+	```js
+	npm install ukkio-sdk
+	```
+
+	or clone the git repository
+
+
+- Include SDK in your HTML
+
+	```html
+	<script src="ukkiosdk.js"></script>
+	```
+
+- Start sandbox 
+
+	```
+	$ cd your/game/folder
+	$ ukkio sandbox
+	$ Sandbox started: point your browser at http://localhost:5005/
+	```
+
+- Start programming!
+
+## Understand the game life cycle
+
+In Ukkio the games must be designed to work with coin, in other words you earn money every time a player insert a coin. This is by design.
+The main target of SDK is to give you a simple way to consume the coins plus some basic feautures that could be ignored depending on the
+type of game you are building. 
+
+SDK are based on events. There are several types of events that comes from Ukkio (`newGame`, `pause`, `unpause`, `changeViewport`) and 
+a few methods (`ready`, `insertCoin`, `gameOver`, `exit`) that let you send and retrive data (`saveSession`, `loadSession`, `saveStorage`, `loadStorage`).
+
+When a player start your game Ukkio create an iframe on the page, that cover any UI element on the window, and execute your script.
+After loaded all your stuff you have to say to Ukkio - Hey, I'm `ready`! - using the `ready()` function. Ukkio responds with the `newGame`
+events. From now on you are ready to ask to the player to insert coin using `insertCoin()`, many kind of things could happen after this call: 
+
+1. The user accept (`success`)
+2. The user reject (`cancel`)
+3. The user has no coins (`failure`)
+
+You have to manage all of them. 
+
+For simplicity we go for the first case. The game started and after some levels he lost, now you have to tell to Ukkio that the coin is burned 
+using `gameOver()`. Now you can request a new coin to player but if he wants to go back on the game list (remember?) there is no UI on the screen to do so. 
+Here is where you come in for help drawing a (what you want) button that calls `exit()`.
+
+That would be enough for a certain types of games but if you want let the player to continue from a certain point you need a way to save data. 
+HTML5 has localStorage but it lives only on the device in use. Not enough for a cool HTML5 game. 
+Ukkio provide two methods: `saveStorage` and `loadStorage`. With them you can save and retrive an object from any device. 
+Storage is unique for the couple: player, game. Oh..they work off-line too, look bottom for more informations.
+
+Ukkio has another couple of things to deliver a great user experience: `pause` and `unpause`. Imagine the case when you are playing the
+the latest browser's game (Galaga) at the office (Helicarrier) and your boss (Tony Stark) knocks on the door and you quickly press 
+CMD+TAB (ALT+TAB for others). What happend to your game (Game over!)? 
+To avoid this catastrophic scenario Ukkio send a `pause` events when window lost the focus. Pause has a callback that accept a data object
+that is stored first on the localStorage and then on the server. So you can close browser and continue the game more late (when Tony is gone away).
+`unpause`, instead, is triggered when window gets the focus. All previously stored data are passed as parameter. If you want to read or write
+pause and unpause data at anytime you can use `saveSession()` and `loadSession()` but be careful, that data lives like a coin. 
+When you call `gameOver()` session is deleted immediatly.
+
+
+## Events
+
+### newGame(data)
+
+Invoked after a `ready` call. The game is ready to start. Data passed from Ukkio contains the game mode (`new`, `resume`) and the player's data.
+
+```js
+var ukkio = new UkkioSDK({
+	apiKey: 'myAPIKey'
+});
+
+ukkio.on('newGame', function (gameData) {
+	// Game is ready to start
+	main();
+});
+
+// Tells to Ukkio the game is ready. 
+// Options works only in the sandbox, in the production they are ignored.
+ukkio.ready({
+	verbose: true,
+	user: { coins: 10 }
+});
+```
+
+Data contains:
+
+- `mode`: can be `new` or `resume`. If `new`, this is a new game and it require an `insertCoin()` to start. If `resume`, the game was paused so no
+  game over occurred. In this case you have to restore session data and start the game from the saved point. Use `session` key for this. 
+  Call `insertCoin()` in this case will throw an error.
+- `user`: contains player's data.
+
+	```js
+	user: {
+		"username": "gamedeveloper",
+		"coins": 100,
+		"locale": "en",
+		"timezone": '2'
+	}
+	```
+
+- `session`: null if `mode` is equal to `new`. It contains data stored on `pause` events or using `saveSession()`.
+- `storage`: it contains persistent data stored using `saveStorage()`.
+
+
+### pause(callback)
+
+Invoked after the game container lost the focus. Callback(gameData) accepts an object with the necessary data to resume the game in a second time.
+
+```js
+ukkio.on('pause', function (callback) {
+	// On pause saves score and birdie's position to be 
+	// restored in another device (mobile phone?).
+	callback({
+		score: score,
+		birdie: {
+			x: birdie.x,
+			y: birdie.y,
+			angle: birdie.angle
+		}
+	});
+});
+```
+
+### unpause(gameData)
+
+Invoked after the game container gets the focus. `gameData` contains exactly the object you stored on `pause`.
+
+```js
+ukkio.on('unpause', function (data) {
+	// On unpause restores data of the last game like score
+	// and birdie position. 
+	// Important! This code has no effects, take it as a pure example.
+	score = data.score;
+	birdie.x = data.birdie.x;
+	birdie.y = data.birdie.y;
+	birdie.angle = data.birdie.angle;
+});
+```
+
+### changeViewport(viewport)
+
+Invoked when the window is resized or smartphone change orientation. Created because some framework has problems to detect
+resize or orientation changes when inside an iframe.
+
+```js
+ukkio.on('changeViewport', function (viewport) {
+	stage.width = viewport.width;
+	stage.height = viewport.height;
+});
+```
+
+
+
+## Methods
+
+### ready(options)
+
+Call this method when you are ready to recive player's informations. That method start the Ukkio life cycle. 
+The parameter `options` works only in the sandbox and is useful to test session, storage, not enough coins, etc...
+
+```js
+ukkio.ready({
+	verbose: true,
+	user: { coins: 0 },
+	session: { foo: 'bar' },
+	storage: { persistenFoo: 'persistentBar' }
+});
+```
+
+Options:
+
+- `user`: you overwrite user's profile with custom data.
+
+	```js
+	user: {
+		"username": "gamedeveloper",
+		"coins": 100,
+		"locale": "en",
+		"timezone": '2'
+	}
+	```
+
+- `session`: custom data for the session. If session is not null `newGame` events receives a `mode: 'resume'`
+- `storage`: custom data for the storage.
+- `verbose`: true or false. If true any interaction with the SDK will be shown in the console.
+
+### insertCoin(callback)
+
+Asks to the player to insert a coin. In the sandbox a coin is consumed immediatly without user's interactions. 
+On Ukkio a "fullscreen" dialog asks to insert a coin. Player has the capability to accept or reject.
+Callback(result) passes a parameter with the result string of transaction.
+
+```js
+ukkio.insertCoin(function (result) {
+	if (result == 'success') {
+		start();
+	}
+	else {
+		mainMenu();
+	}
+});
+```
+
+Result can be:
+
+- `success`: transaction is completed. You earned money!!
+- `failure`: not enough coins.
+- `cancel`: player has rejected.
+
+### gameOver(callback)
+
+Call it when player ended his game section. Remember, only after game over you can ask for a new coin.
+Callback() has no parameters.
+
+```js
+ukkio.gameOver(function () {
+	gameOver = true;
+	...
+	ukkio.saveStorage({ hiscore: hiscore }, function() {
+		...
+		mainMenu();
+	}
+
+```
+
+### exit()
+
+Calls it when a user want to go back to the game list. To let you use all pixels on the screen we don't inserts a toolbar, a banner 
+or something else. For this we ask to you to handle the exit button. The only action to do is to call ukkio.exit() and the iframe
+in which the game is loaded will be deleted from the page. Remember to save all before exit. 
+
+If game is over consider this:
+
+```js
+ukkio.saveStorage({ some: 'data' }, function () {
+	ukkio.exit();
+});
+```
+
+If game still running:
+
+```js
+ukkio.saveSession({ some: 'data' }, function () {
+	ukkio.exit();
+});
+```
+
+or the player will lose the game's status.
+
+### saveStorage(data, callback)
+
+### loadStorage(callback)
+
+### saveSession(data, callback)
+
+### loadSession(data)
+
+
+## References
+
+Visit developer site for more informations on Ukkio: [http://developer.ukk.io](http://developer.ukk.io)
+
+
+## License
+
+```
+The MIT License (MIT)
+
+Copyright (c) 2014 Ukkio
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+```
---
+++ ukkiosdk.js
@@ -0,0 +1,403 @@
+;(function () {
+
+var triggerEvents = function(events, args) {
+	var ev, i = -1, l = events.length, a1 = args[0], a2 = args[1], a3 = args[2];
+		switch (args.length) {
+		case 0: while (++i < l) (ev = events[i]).callback.call(ev.ctx); return;
+		case 1: while (++i < l) (ev = events[i]).callback.call(ev.ctx, a1); return;
+		case 2: while (++i < l) (ev = events[i]).callback.call(ev.ctx, a1, a2); return;
+		case 3: while (++i < l) (ev = events[i]).callback.call(ev.ctx, a1, a2, a3); return;
+		default: while (++i < l) (ev = events[i]).callback.apply(ev.ctx, args);
+	}
+}
+
+// Underscore utilities
+var arrayKeys = Object.keys || function (obj) {
+	if (obj !== Object(obj)) throw new TypeError('Invalid object');
+	var keys = [];
+	for (var key in obj) if (_.has(obj, key)) keys.push(key);
+	return keys;
+}
+
+var slice = Array.prototype.slice;
+var bind = function(func, context) {
+	var args, bound;
+	if (Function.prototype.bind && func.bind === nativeBind) return nativeBind.apply(func, slice.call(arguments, 1));
+	args = slice.call(arguments, 2);
+	return bound = function() {
+		if (!(this instanceof bound)) return func.apply(context, args.concat(slice.call(arguments)));
+		ctor.prototype = func.prototype;
+		var self = new ctor;
+		ctor.prototype = null;
+		var result = func.apply(self, args.concat(slice.call(arguments)));
+		if (Object(result) === result) return result;
+		return self;
+	}
+}
+
+// Wrapper of addEventListener
+var onWindowEvent = function onWindowEvent(eventName, callback, context) {
+	if (typeof context === 'undefined')
+		context = window;
+
+	var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+	var eventer = window[eventMethod];
+
+	eventer(eventName, function() {
+		callback.apply(context, arguments);
+	}, false);
+}
+
+// Utility to post message to iFrame
+var PostMessage = function PostMessage(options) {
+	this.initialize(options);
+}
+
+// options = {
+//   target: window.parent or iframe.contentWindow
+//   origin: Host of your application
+//   destination: Host where the message are sent (Ex. http://example.com)
+// }
+PostMessage.prototype.initialize = function initialize(options) {
+	if (!options.target)
+		return console.error('You have to specify the target options to post a message.');
+	if (!options.origin)
+		return console.error('You have to specify the origin options for security reasons.');
+	if (!options.destination)
+		return console.error('You have to specify the destination options for security reasons.');
+
+	var self = this;
+	self._events = [];
+	self._callbacks = {};
+	self._callbackCounter = 0;
+	self.options = options;
+
+	self._target = options.target;
+	delete options['target'];
+
+	var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+	var eventer = window[eventMethod];
+	var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+	self._messageEventHandler = function() {
+		self._messageHandler.apply(self, arguments);
+	};
+	eventer(messageEvent, self._messageEventHandler, false);
+}
+
+PostMessage.prototype._messageHandler = function _messageHandler(message) {
+	// console.log('Raw message ' + this.options.name, message);
+
+	if (message.origin !== this.options.destination)
+		return console.warn('Message coming from a different origin: ' + message.origin + ' => ' + this.options.destination);
+
+	var type = message.data.type;
+	var data = message.data.data;
+	var callbackId = message.data.callbackId;
+
+	if (!type)
+		return console.warn('Unknown message type');
+
+	switch (type) {
+
+		case 'callback':
+			if (!callbackId)
+				return console.warn('No callbackId provided on callback message');
+			if (!this._callbacks[callbackId])
+				return console.warn('Cannot find a callback with ID ' + callbackId);
+
+			var callback = this._callbacks[callbackId];
+			delete this._callbacks[callbackId];
+			callback(data);
+			break;
+
+		default:
+			if (callbackId && data !== undefined)
+				this.trigger(type, data, this._createCallbackFunction(callbackId));
+			else if (callbackId)
+				this.trigger(type, this._createCallbackFunction(callbackId));
+			else if (data !== undefined)
+				this.trigger(type, data);
+			else
+				this.trigger(type);
+	}
+}
+
+PostMessage.prototype._createCallbackFunction = function _createCallbackFunction(callbackId) {
+	var self = this;
+	return function (data) {
+		var newMessage = {
+			type: 'callback',
+			data: data,
+			callbackId: callbackId
+		}
+		self._target.postMessage(newMessage, self.options.destination);
+	}
+}
+
+PostMessage.prototype.emit = function emit(type, data, callback) {
+	if (typeof data === 'function') {
+		callback = data;
+		data = undefined;
+	}
+
+	if (typeof callback === 'function') {
+		var callbackId = ++this._callbackCounter;
+		this._callbacks[callbackId] = callback;
+	}
+
+	var newMessage = {
+		type: type,
+		data: data,
+		callbackId: callbackId
+	}
+
+	this._target.postMessage(newMessage, this.options.destination);
+}
+
+PostMessage.prototype.on = function on(name, callback, context) {
+	if (!callback) return this;
+	var events = this._events[name] || (this._events[name] = []);
+	events.push({ callback: callback, context: context, ctx: context || this });
+	return this;
+}
+
+PostMessage.prototype.trigger = function trigger(name) {
+	if (!this._events) return this;
+	var args = [].slice.call(arguments, 1);
+	var events = this._events[name];
+	if (events) triggerEvents(events, args);
+	return this;
+}
+
+PostMessage.prototype.destroy = function destroy() {
+	var eventMethod = window.removeEventListener ? "removeEventListener" : "detachEvent";
+	var eventer = window[eventMethod];
+	var messageEvent = eventMethod == "detachEvent" ? "onmessage" : "message";
+	eventer(messageEvent, this._messageEventHandler, false);
+}
+
+
+
+
+// UkkioSDK
+var UkkioSDK = function UkkioSDK(options) {
+	this.initialize(options);
+}
+
+
+UkkioSDK.prototype.initialize = function initialize(options) {
+	var self = this;
+	if (window.self === window.top)
+		return self.handleErrors('UkkioSDK must be executed under sandbox or play room. For more informations see http://developer.ukk.io');
+
+	self._parent = window.parent;
+	self._events = {};
+
+	if (typeof options === 'undefined')
+		self.options = {};
+	else
+		self.options = options;
+
+	if (!self.options.origin) {
+		self.options.origin = window.location.protocol + '//' + window.location.host;
+		if (parseInt(window.location.port) != 80)
+			self.options.origin += ':' + window.location.port;
+	}
+
+	if (!self.options.destination) {
+		self.options.destination = window.location.protocol + '//' + window.location.hostname + ':' + (parseInt(window.location.port) - 1);
+		if (parseInt(window.location.port) != 80)
+			self.options.destination += ':' + (parseInt(window.location.port) - 1);
+	}
+
+	console.log(self.options);
+
+	self._postMessage = new PostMessage({
+		name: 'client',
+		target: window.parent,
+		origin: self.options.origin,
+		destination: self.options.destination
+	});
+
+	self._postMessage.on('newGame', this.onNewGame, self);
+	self._postMessage.on('changeViewport', this.onChangeViewport, self);
+	self._postMessage.on('pause', this.onPause, self);
+	self._postMessage.on('unpause', this.onUnpause, self);
+
+	onWindowEvent('focus', this.onFocus, self);
+	onWindowEvent('blur', this.onBlur, self);
+}
+
+// Backbone implementation
+UkkioSDK.prototype.on = function on(name, callback, context) {
+	if (!callback) return this;
+	var events = this._events[name] || (this._events[name] = []);
+	events.push({ callback: callback, context: context, ctx: context || this });
+	return this;
+}
+
+// Backbone implementation
+UkkioSDK.prototype.off = function off(name, callback, context) {
+	var retain, ev, events, names, i, l, j, k;
+	if (!name && !callback && !context) {
+		this._events = {};
+		return this;
+	}
+
+	names = name ? [name] : arrayKeys(this._events);
+	for (i = 0, l = names.length; i < l; i++) {
+		name = names[i];
+		if (events = this._events[name]) {
+			this._events[name] = retain = [];
+			if (callback || context) {
+				for (j = 0, k = events.length; j < k; j++) {
+					ev = events[j];
+					if ((callback && callback !== ev.callback && callback !== ev.callback._callback) ||
+						(context && context !== ev.context)) {
+						retain.push(ev);
+					}
+				}
+			}
+			if (!retain.length) delete this._events[name];
+		}
+	}
+
+	return this;
+}
+
+// Backbone implementation
+UkkioSDK.prototype.trigger = function trigger(name) {
+	if (!this._events) return this;
+	var args = [].slice.call(arguments, 1);
+	var events = this._events[name];
+	if (events) triggerEvents(events, args);
+	return this;
+}
+
+// Send a message to the host check if the service is ready
+UkkioSDK.prototype.ready = function ready(settings) {
+	var data;
+	data = settings || {};
+	data.apiKey = this.options.apiKey;
+	this._postMessage.emit('ready', data);
+	return this;
+}
+
+// Request an insert coin
+UkkioSDK.prototype.insertCoin = function insertCoin(fn) {
+	this._postMessage.emit('insertCoin', function (result) {
+		fn(result);
+	});
+	return this;
+}
+
+// Save custom data into the current game slot. This data
+// will be lost on a new game.
+UkkioSDK.prototype.saveSession = function saveSession(data, fn) {
+	this._postMessage.emit('saveSession', data, function (result) {
+		if (typeof fn === 'function')
+			fn(result);
+	});
+	return this;
+}
+
+// Load custom data for the current match
+UkkioSDK.prototype.loadSession = function loadSession(fn) {
+	this._postMessage.emit('loadSession', function (data) {
+		if (typeof fn === 'function')
+			fn(data);
+	});
+	return this;
+}
+
+// Save user's persistent data
+UkkioSDK.prototype.saveStorage = function saveStorage(data, fn) {
+	this._postMessage.emit('saveStorage', data, function (result) {
+		if (typeof fn === 'function')
+			fn(result);
+	});
+	return this;
+}
+
+// Load user's persistent data
+UkkioSDK.prototype.loadStorage = function loadStorage(fn) {
+	this._postMessage.emit('loadStorage', function (data) {
+		if (typeof fn === 'function')
+			fn(data);
+	});
+	return this;
+}
+
+// The game is over
+UkkioSDK.prototype.gameOver = function gameOver(fn) {
+	this._postMessage.emit('gameOver', function (result) {
+		if (typeof fn === 'function')
+			fn(result);
+	});
+	return this;
+}
+
+// Exit the game
+UkkioSDK.prototype.exit = function exit() {
+	this._postMessage.emit('exit');
+}
+
+// Ukkio event of type newGame is coming
+UkkioSDK.prototype.onNewGame = function onNewGame(data) {
+	this.trigger('newGame', data);
+}
+
+// Request pause sending data to save the state of the game
+UkkioSDK.prototype.onPause = function onPause(fn) {
+	this.trigger('pause', function (gameData) {
+		fn(gameData);
+	});
+}
+
+// Request of resume the game. The last saved state is returned
+UkkioSDK.prototype.onUnpause = function onUnpause(gameData) {
+	this.trigger('unpause', gameData);
+}
+
+// Window or rotation is changed
+UkkioSDK.prototype.onChangeViewport = function onChangeViewport(viewport) {
+	this.trigger('changeViewport', viewport);
+}
+
+// Handle warning message comes from SDK
+UkkioSDK.prototype.handleWarnings = function handleWarnings(msg) {
+	if (console.warn) console.warn(msg);
+	return this;
+}
+
+// Handle warning message comes from SDK
+UkkioSDK.prototype.handleErrors = function handleErrors(msg) {
+	if (console.error) console.error(msg);
+	return this;
+}
+
+// Window get focus
+UkkioSDK.prototype.onFocus = function onFocus(e) {
+	this._postMessage.emit('active', true);
+}
+
+// Window lost focus
+UkkioSDK.prototype.onBlur = function onBlur(e) {
+	this._postMessage.emit('active', false);
+}
+
+// Clean up handlers
+UkkioSDK.prototype.destroy = function destroy() {
+	this._postMessage.destroy();
+}
+
+if (typeof exports == 'object') {
+	exports = module.exports = UkkioSDK;
+} else if (typeof define == 'function' && define.amd) {
+	define(function(){ return UkkioSDK; });
+} else {
+	window['UkkioSDK'] = UkkioSDK;
+}
+
+})();
